3f54181403b5fe4a808d5b7fcde16f9d9c482653
Alexander Zhukov <zhukovap@ornl.gov>
2024-01-10 22:49:42 -0500

    Added venv creation and PyORBIT wheel installation

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 HERE="$( dirname "${BASH_SOURCE[0]}" )"
 cp $HERE/pycharm.desktop $HOME/.local/share/applications
+
+OLD=$(pwd)
+
+cd $HERE
+python -m venv ../env
+source ../env/bin/activate
+pip install -U pip
+pip install numpy scipy matplotlib pcaspy pyepics
+pip install PyORBIT-0.1.dev1199+g62416c4-cp310-cp310-linux_x86_64.whl
+cd $OLD
+
