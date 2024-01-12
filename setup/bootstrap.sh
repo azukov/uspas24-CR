3603477e9474e2c8270e9c8144dcd3016c142fcb
Zhukov, Alexander P <zhukovap@ornl.gov>
2024-01-12 15:13:56 -0500

    VA package added

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -8,7 +8,8 @@ cd $HERE
 python -m venv ../env
 source ../env/bin/activate
 pip install -U pip
-pip install numpy scipy matplotlib pcaspy pyepics
+pip install numpy scipy matplotlib pcaspy pyepics pyside6 jupyter
 pip install PyORBIT-0.1.dev1199+g62416c4-cp310-cp310-linux_x86_64.whl
+pip install virtaccl-0.0.0-py3-none-any.whl
 cd $OLD
 
