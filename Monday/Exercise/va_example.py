7e1ffd5373ff44e11646f3aa2b3ef2f69a33e55e
zhukovap <zhukovap@U-28V8PQQZ2LP7Q.uspasDevSimpleAD.fnal.gov>
2024-01-29 00:37:14 -0500

    Preparation for Monday

---
+++ Monday/Exercise/va_example.py
@@ -0,0 +1,43 @@
+# This is an example script using the virtual accelerator.
+
+# Importing epics to use pyepics to talk to Epics
+import epics
+
+# Importing time to allow our script to sleep while we wait for changes to be registered.
+import time
+
+# Imports to help with plotting.
+import numpy as np
+from matplotlib import pyplot as plt
+
+# PV names for the cavity we want to change.
+cavity_name = "SCL_LLRF:FCM23d"
+# PV name for the BPM we want to read.
+BPM_name = "SCL_Diag:BPM32"
+
+# Here we grab the initial cavity phase. "CtlPhaseSet" is added to the cavity name to specify the phase.
+initial_phase = epics.caget(cavity_name + ":CtlPhaseSet")
+
+# Setup for the scan to make a plot later.
+num = 11
+cavity_phases = np.linspace(-10 + initial_phase, 10 + initial_phase, num)
+bpm_phases = np.zeros(num)
+
+for i in range(len(cavity_phases)):
+    # Set the cavity phase
+    epics.caput(cavity_name + ":CtlPhaseSet", cavity_phases[i])
+
+    # Sleep long enough for the cavity to change.
+    time.sleep(1.1)
+
+    # Read the arrival phase of the BPM after sleeping long enough for it to update.
+    bpm_phases[i] = epics.caget(BPM_name + ":phaseAvg")
+
+    print(cavity_phases[i], bpm_phases[i])
+
+# Return the cavity phase to it's original value.
+epics.caput(cavity_name + ":CtlPhaseSet", initial_phase)
+
+# Plot the results.
+plt.plot(cavity_phases, bpm_phases, 'o')
+plt.show()
---
+++ Monday/Homework/README.md
@@ -0,0 +1,4 @@
+For HW4 laucnh VA wiht
+```
+slit_va --debug --wedge 30 --si 1
+```
