664ff3e1a4ef70a9206b5e4c32176410254297af
Sasha  Zhukov <zhukovap@ornl.gov>
2024-01-29 10:55:48 -0500

    Monday again

--- Monday/Exercise/va_example.py
+++ Monday/Exercise/va_example.py
@@ -23,12 +23,13 @@ num = 11
 cavity_phases = np.linspace(-10 + initial_phase, 10 + initial_phase, num)
 bpm_phases = np.zeros(num)
 
+print("Cavity Phase [degrees]    BPM Phase [degrees]")
 for i in range(len(cavity_phases)):
     # Set the cavity phase
     epics.caput(cavity_name + ":CtlPhaseSet", cavity_phases[i])
 
     # Sleep long enough for the cavity to change.
-    time.sleep(1.1)
+    time.sleep(1.5)
 
     # Read the arrival phase of the BPM after sleeping long enough for it to update.
     bpm_phases[i] = epics.caget(BPM_name + ":phaseAvg")
@@ -40,4 +41,6 @@ epics.caput(cavity_name + ":CtlPhaseSet", initial_phase)
 
 # Plot the results.
 plt.plot(cavity_phases, bpm_phases, 'o')
+plt.xlabel("Cavity Phase [degrees]")
+plt.ylabel("BPM Phase [degrees]")
 plt.show()
